7fbbd5798af7ba8269f510646376f776621c66b9
Chris Coleman <chriscoleman@executivehomes.com>
2024-12-04 15:36:48 -0600

    testing routing in Sigma

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,12 @@ import "./App.css";
 
 // Import components for routes
 import { GanttChart } from "./plugins/index";
-import { Home } from "./pages/Home";
 
 export function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<GanttChart />} />
         <Route path="/gantt-chart" element={<GanttChart />} />
       </Routes>
     </Router>
--- src/plugins/gantt-chart/GanttChart.tsx
+++ src/plugins/gantt-chart/GanttChart.tsx
@@ -1,7 +1,44 @@
 import { ChartContainer } from "./components";
 import { tasks } from "./tasks";
+import {
+  client,
+  useConfig,
+  useElementData,
+  useElementColumns,
+} from "@sigmacomputing/plugin";
+
+client.config.configureEditorPanel([
+  { name: "data source!!!!!!", type: "element" },
+  {
+    name: "dimension",
+    type: "column",
+    source: "data source",
+    allowMultiple: true,
+  },
+  {
+    name: "measures!!!!",
+    type: "column",
+    source: "data source",
+    allowMultiple: true,
+  },
+  { name: "a group of values!!!", type: "group" },
+  { name: "text input #1", source: "a group of values", type: "text" },
+  { name: "text input #2", source: "a group of values", type: "text" },
+  { name: "apiKey", type: "text", secure: true },
+]);
 
 export const GanttChart = () => {
+  const config = useConfig();
+
+  const columnInfo = useElementColumns(config.source);
+
+  // arrays of the ids corresponding to the "dimension" and "measures" data columns from the editor panel
+  const { dimension, measures } = config;
+
+  console.log("columnInfo", columnInfo);
+  console.log("dimension", dimension);
+  console.log("measures", measures);
+
   return (
     <div className="App">
       <header>
